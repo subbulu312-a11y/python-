f3ce489ecf0c22730d04bed95570199f6605ba3e
subbulu312-a11y <subbulu312@gmail.com>
2026-06-02 15:34:05 +0530

    add

--- py7.py
+++ py7.py
@@ -1,6 +1,11 @@
-def squares_list(n):
+'''def squares_list(n):
     result=[]
     for i in range(n):
         result.append(i*i)
     return result
-print(squares_list(5))
+print(squares_list(5))'''
+def squares_gen(n):
+    for i in range(n):
+        yield i*i
+gen=squares_gen(5)
+print(list(gen))
