d71fcdc15c995c418c1ddd60a164f6a42a3174fe
subbulu312-a11y <subbulu312@gmail.com>
2026-06-02 12:51:00 +0530

    add it

--- py6.py
+++ py6.py
@@ -1,7 +1,17 @@
-def seasons():
+'''def seasons():
     yield 'Spring'
     yield 'Summer'
     yield 'Autumn'
     yield 'Winter'
 for s in seasons():
-    print(s)
+    print(s)'''
+from tokenize import generate_tokens
+
+
+def natural_numbers():
+    n=1
+    while True:
+        yield n
+        n +=1
+gen=natural_numbers()
+print([next(gen) for _ in range(6)])
