7727a3ee5833665d945c2f674c177324b7ea3eb0
subbulu312-a11y <subbulu312@gmail.com>
2026-06-08 15:44:29 +0530

    add

---
+++ script19.py
@@ -0,0 +1,27 @@
+'''def s(n):
+    return n*n
+r=s(5)
+print(r)'''
+'''def greet(name):
+    print("hello",+ name+"!")
+print("subbu")
+print("bob")
+def fun(a,b,c):
+    return a*b*c
+result=fun(1,2,3)
+print(result)
+def pet(animal,name):
+    print(f"my {animal} is named {name}")
+pet(animal="dog",name="cat")
+def full_name(first,middle,last):
+    print(f"{first} {middle} {last}")
+full_name(first="Andy",middle="Bob",last="Susan")'''
+def power(base,exponent):
+    return base**exponent
+result=power(2,3)
+print(result)
+
+
+
+
+
